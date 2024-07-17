48468dbbffa6c0441818dbb2b9c23f261300e565
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-07-17 12:50:35 +0530

    Final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,21 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        dataBinding=true
+    }
+    androidResources{
+        noCompress; "tflite"
+        noCompress; "lite"
+    }
 }
-
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.tensorflow.lite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/starkindustries/regressionmodel/MainActivity.kt
+++ app/src/main/java/com/starkindustries/regressionmodel/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.starkindustries.regressionmodel
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.starkindustries.regressionmodel.databinding.ActivityMainBinding
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var interpreter:Interpreter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        interpreter=Interpreter(loadModelFile())
+        binding.resultButton.setOnClickListener()
+        {
+            val view=this.currentFocus
+            if(view!=null)
+            {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(view.windowToken,0)
+            }
+            binding.result.setText("Result:"+doInference(binding.data.text.toString().trim()))
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+    fun doInference(`val`: String): Float {
+        val input = FloatArray(1)
+        input[0] = `val`.toFloat()
+        val output = Array(1) { FloatArray(1) }
+        interpreter.run(input, output)
+        return output[0][0]
+    }
+    @Throws(IOException::class)
+    private fun loadModelFile(): MappedByteBuffer {
+        val assetFileDescriptor = this.assets.openFd("linear.tflite")
+        val fileInputStream = FileInputStream(assetFileDescriptor.fileDescriptor)
+        val fileChannel = fileInputStream.channel
+        val startOffset = assetFileDescriptor.startOffset
+        val length = assetFileDescriptor.getLength()
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, length)
+    }
+
 }
